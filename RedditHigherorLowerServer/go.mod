2083723dc91bb09edb33a30c9bf4d91d81bfb24f
Vroom <harrison.hilpert@gmail.com>
2024-08-14 11:18:28 -0700

    Init Commit

---
+++ RedditHigherorLowerServer/go.mod
@@ -0,0 +1,3 @@
+module RedditHigherorLowerServer
+
+go 1.23.0
---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+];

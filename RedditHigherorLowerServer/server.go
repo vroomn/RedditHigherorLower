4cccf7e3d5d9b3419aa93b4f8f4358f3d9468acc
Vroom <harrison.hilpert@gmail.com>
2024-08-14 12:08:27 -0700

    Get Json Data and format

--- RedditHigherorLowerServer/server.go
+++ RedditHigherorLowerServer/server.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello Server 2!")
+	response, err := http.Get("https://www.reddit.com/reddits.json?count=1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	headerBytes, err := json.MarshalIndent(response.Header, " ", "   ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(headerBytes))
+	fmt.Println(response.StatusCode)
+
 }
